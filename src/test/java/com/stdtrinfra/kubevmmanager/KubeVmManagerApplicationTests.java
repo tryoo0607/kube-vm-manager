22f67b4b0418ff6406194cef4d1da9c3c07c0948
tyroo0607 <tryoo0607@gmail.com>
2024-02-08 21:49:26 +0900

    Initial commit

---
+++ src/test/java/com/stdtrinfra/kubevmmanager/KubeVmManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdtrinfra.kubevmmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeVmManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
