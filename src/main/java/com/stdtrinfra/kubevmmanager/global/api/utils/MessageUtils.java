0bae9d3cccceddafd4722846980aff5426d795f6
tyroo0607 <tryoo0607@gmail.com>
2024-02-09 16:37:21 +0900

    Initial Settings 2 - Settings for Locale

---
+++ src/main/java/com/stdtrinfra/kubevmmanager/global/api/utils/MessageUtils.java
@@ -0,0 +1,35 @@
+package com.stdtrinfra.kubevmmanager.global.api.utils;
+
+import com.stdtrinfra.kubevmmanager.global.api.model.ResponseMessage;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class MessageUtils implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        MessageUtils.applicationContext = applicationContext;
+    }
+
+    public static ResponseMessage getMessage(String code){
+        MessageSource messageSource = applicationContext.getBean("customMessageSource", MessageSource.class);
+        String message = messageSource.getMessage(code, null, Locale.KOREA);
+
+        return ResponseMessage.builder().message(message).code(code).build();
+    }
+
+    public static ResponseMessage getMessage(String code, Object[] args){
+        MessageSource messageSource = applicationContext.getBean("customMessageSource", MessageSource.class);
+        String message = messageSource.getMessage(code, args, Locale.KOREA);
+
+        return ResponseMessage.builder().message(message).code(code).build();
+    }
+}
+
